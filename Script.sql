e91f79540423ca4639801d1407b38d8085143cbe
azaLiza <liza_boumali@hotmail.com>
2021-12-03 00:16:23 +0100

    update script

--- Script.sql
+++ Script.sql
@@ -10,7 +10,8 @@ Create table Utilisateur(
     pseudo Varchar(20),
     mail Varchar(20),
     motDePasse Varchar(20),
-    access numeric(1) DEFAULT 1, -- 0 Admin /  1 User
+    access numeric(1) DEFAULT 1
+    -- 0 Admin /  1 User
 );
 
 create table Sauveteur(
