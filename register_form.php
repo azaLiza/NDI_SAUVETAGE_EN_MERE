4283ad18a736da95c3c5f109381c27748beb81e9
azaLiza <liza_boumali@hotmail.com>
2021-12-03 00:07:22 +0100

    Update register form

--- register_form.php
+++ register_form.php
@@ -17,7 +17,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
   $confirmPassword = $_POST["confirmPassword"];
 
   // Check if the user exists dba_firstkey
-  $sqlGetUsers = "SELECT * FROM users WHERE username='$username'";
+  $sqlGetUsers = "SELECT * FROM Utilisateur WHERE username='$username'";
 
   $result = mysqli_query($conn, $sqli);
   $num = mysqli_num_rows($result);
@@ -25,6 +25,27 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
   // This sql query is use to check if
     // the username is already present
     // or not in our Database
-    
+    if ($num == 0) {
+      // code...
+      if (($password == $confirmPassword) && $exists == false) {
+        // code...
+        $hash = password_hash($password, PASSWORD_DEFAULT);
+        $sqlInsertUser = "INSERT INTO 'Utilisateur' ( 'pseudo', 'mail', 'motDePasse') values ('$username', '$email', '$hash')";
+
+        $result = mysqli_query($conn, $sqlInsertUser);
+
+        if ($result) {
+          // code...
+          $showAlert = true;
+        }
+      } else {
+        $showError = "Passwords do not match";
+      }
+    }
+
+    if ($num > 0) {
+      // code...
+      $exists = "The username is not available";
+    }
 }
  ?>
