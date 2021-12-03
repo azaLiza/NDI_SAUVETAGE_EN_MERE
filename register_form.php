041b6881f743a0f0218dc4e01a27078668a870cf
azaLiza <liza_boumali@hotmail.com>
2021-12-03 01:21:56 +0100

    Update register form inserting users in DB

--- register_form.php
+++ register_form.php
@@ -17,9 +17,9 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
   $confirmPassword = $_POST["confirmPassword"];
 
   // Check if the user exists dba_firstkey
-  $sqlGetUsers = "SELECT * FROM Utilisateur WHERE username='$username'";
+  $sqlGetUsers = "SELECT * FROM Utilisateur WHERE pseudo='$username'";
 
-  $result = mysqli_query($conn, $sqli);
+  $result = mysqli_query($conn, $sqlGetUsers);
   $num = mysqli_num_rows($result);
 
   // This sql query is use to check if
@@ -30,7 +30,9 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
       if (($password == $confirmPassword) && $exists == false) {
         // code...
         $hash = password_hash($password, PASSWORD_DEFAULT);
-        $sqlInsertUser = "INSERT INTO 'Utilisateur' ( 'pseudo', 'mail', 'motDePasse') values ('$username', '$email', '$hash')";
+
+        $sqlInsertUser = "INSERT INTO Utilisateur (pseudo, mail, motDePasse, access) VALUES ('$username', 'mail', '$password', 1)";
+
 
         $result = mysqli_query($conn, $sqlInsertUser);
 
@@ -49,3 +51,100 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     }
 }
  ?>
+
+ <!doctype html>
+
+<html lang="en">
+
+<head>
+
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content=
+        "width=device-width, initial-scale=1,
+        shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href=
+"https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css"
+        integrity=
+"sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk"
+        crossorigin="anonymous">
+</head>
+
+<body>
+
+<?php
+
+    if($showAlert) {
+
+        echo ' <div class="alert alert-success
+            alert-dismissible fade show" role="alert">
+
+            <strong>Success!</strong> Your account is
+            now created and you can login.
+            <button type="button" class="close"
+                data-dismiss="alert" aria-label="Close">
+                <span aria-hidden="true">×</span>
+            </button>
+        </div> ';
+    }
+
+    if($showError) {
+
+        echo ' <div class="alert alert-danger
+            alert-dismissible fade show" role="alert">
+        <strong>Error!</strong> '. $showError.'
+
+       <button type="button" class="close"
+            data-dismiss="alert aria-label="Close">
+            <span aria-hidden="true">×</span>
+       </button>
+     </div> ';
+   }
+
+    if($exists) {
+        echo ' <div class="alert alert-danger
+            alert-dismissible fade show" role="alert">
+
+        <strong>Error!</strong> '. $exists.'
+        <button type="button" class="close"
+            data-dismiss="alert" aria-label="Close">
+            <span aria-hidden="true">×</span>
+        </button>
+       </div> ';
+     }
+
+?>
+
+<div class="container my-4 ">
+
+    <h1 class="text-center">Signup Here</h1>
+    <form action="register_form.php" method="POST">
+
+        <div class="form-group">
+            <label for="username">Username</label>
+        <input type="text" class="form-control" id="username"
+            name="username" aria-describedby="emailHelp">
+        </div>
+
+        <div class="form-group">
+            <label for="password">Password</label>
+            <input type="password" class="form-control"
+            id="password" name="password">
+        </div>
+
+        <div class="form-group">
+            <label for="cpassword">Confirm Password</label>
+            <input type="password" class="form-control"
+                id="confirmPassword" name="confirmPassword">
+        </div>
+
+        <button type="submit" class="btn btn-primary">
+        SignUp
+        </button>
+    </form>
+</div>
+
+</body>
+</html>
